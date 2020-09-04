604f9b9cfa0c75fea2137942e226febd79154321
amgad <amgad.abdelkhaleq@gmail.com>
2020-09-05 01:47:57 +0200

    create product Jpa

---
+++ 02-backend/spring-boot-ecommerce/src/test/java/com/luvtocode/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luvtocode.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
